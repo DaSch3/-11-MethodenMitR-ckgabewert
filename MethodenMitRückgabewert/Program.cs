a4f1f949bf78019f7d3a894e37498bc3c1c69524
David Schmitz <d.schmitz@dasch3.de>
2021-09-04 19:29:13 +0200

    Projektdateien hinzufügen.

---
+++ MethodenMitRückgabewert/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodenMitRückgabewert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Subtraktion(10, 5));
+            Console.ReadKey();
+        }
+
+        static int Subtraktion(int zahl1, int zahl2)
+        {
+            int ergebnis = zahl1 - zahl2;
+            return ergebnis;
+        }
+    }
+}
